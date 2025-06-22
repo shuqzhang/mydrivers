53cf4f5ae5b90b9a3487447c4b483647b1fb33a8
Shuqing Zhang <linuxshuqzhang@outlook.com>
2025-06-22 19:48:11 +0800

    [none] scripts

--- scull/scull_load.sh
+++ scull/scull_load.sh
@@ -4,3 +4,4 @@ chmod +x scull_load_multi.sh
 ./scull_load_multi.sh scull scull 4
 ./scull_load_multi.sh scull scull_p 4
 ./scull_load_multi.sh scull scull_single 1
+./scull_load_multi.sh scull scull_uid 1
--- scull/scull_unload.sh
+++ scull/scull_unload.sh
@@ -4,3 +4,4 @@ chmod +x scull_unload_multi.sh
 ./scull_unload_multi.sh scull scull 4
 ./scull_unload_multi.sh scull scull_p 4
 ./scull_unload_multi.sh scull scull_single 1
+./scull_unload_multi.sh scull scull_uid 1
